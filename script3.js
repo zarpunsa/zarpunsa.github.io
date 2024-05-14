7225dcbe729ae773d41c0978e10ae59e369fe1d1
zarpunsa <wiwozzz.bamby@gmail.com>
2024-05-14 16:30:14 +0700

    first commit

---
+++ script3.js
@@ -0,0 +1,12 @@
+// const audio = document.querySelector('audio');
+// const body = document.body;
+// <audio autoPlay>
+//   <source src= "anive.mp3" type="audio/mp3">"
+
+// audio.addEventListener('play', () => {
+//   body.classList.add('play-music');
+// });
+
+// audio.addEventListener('pause', () => {
+//   body.classList.remove('play-music');
+// });
---
+++ script4.js
@@ -0,0 +1,10 @@
+$(function(){
+
+    if (!$('.envelope').hasClass('open')){
+      $('.envelope').click(function(){
+        $(this).removeClass('new').addClass('open');
+      });
+  
+  
+    }
+  });
